41ca07e19be4a4c44fddf1a58717788ef857b20a
Armando Alexis Herra Cortez <armandoherra369@gmail.com>
2017-03-31 09:08:26 -0600

    Removed some files and added the TypeScript intro files.

---
+++ typescript/typescript intro/05-Modules/modules.js
@@ -0,0 +1,11 @@
+// TypeScript is modular, we can divide our code up over several files
+// In Angular 2 we then use  "import {} from ''" to access the code in these files
+"use strict";
+exports.__esModule = true;
+// We export a class, interface, variable, ... by adding 'export' keyword in front of it
+var ExportedClass = (function () {
+    function ExportedClass() {
+    }
+    return ExportedClass;
+}());
+exports.ExportedClass = ExportedClass;
