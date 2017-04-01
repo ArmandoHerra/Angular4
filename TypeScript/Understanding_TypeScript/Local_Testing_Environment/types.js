1a15bf7e791b91f7a60e811ada3ef82617d66909
Armando Alexis Herra Cortez <armandoherra369@gmail.com>
2017-03-31 22:41:01 -0600

    Added the types.ts file, file in which I use the different types found in TypeScript.

---
+++ TypeScript/Understanding_TypeScript/Local_Testing_Environment/types.js
@@ -0,0 +1,12 @@
+//String.
+var myName = "Armando";
+//Number.
+var myAge = 21;
+//Boolean.
+var hasHobbies = true;
+//Asign types.
+var myRealAge;
+myRealAge = NaN;
+//Array.
+var hobbies = ["Programming", "Playing Videogames"];
+console.log(hobbies[0]);
---
+++ TypeScript/Understanding_TypeScript/Local_Testing_Environment/types.ts
@@ -0,0 +1,16 @@
+//String.
+let myName: string = "Armando";
+
+//Number.
+let myAge: number = 21;
+
+//Boolean.
+let hasHobbies: boolean = true;
+
+//Asign types.
+let myRealAge: number;
+myRealAge = NaN;
+
+//Array.
+let hobbies: any[] = ["Programming", "Playing Videogames"];
+console.log(hobbies[0]);
