0ed982141bb2e06f0663a60b68f2e34a8212326f
Armando Alexis Herra Cortez <armandoherra369@gmail.com>
2017-04-05 22:41:28 -0500

    Advance on the classes.ts file.

--- TypeScript/Understanding_TypeScript/Local_Testing_Environment/classes.js
+++ TypeScript/Understanding_TypeScript/Local_Testing_Environment/classes.js
@@ -1,10 +1,20 @@
 var Person = (function () {
     function Person(name, username) {
-        this.username = username;
+        this.age = 21;
         this.name = name;
+        this.username = username;
     }
+    Person.prototype.printAge = function () {
+        console.log(this.age);
+    };
+    Person.prototype.setType = function (type) {
+        this.type = type;
+        console.log(this.type);
+    };
     return Person;
 }());
-var person = new Person("Armando", "SekethThoth");
+var person = new Person("Armando", "SekethThoth369");
 console.log(person);
+person.printAge();
+person.setType("Cool guy");
 //# sourceMappingURL=classes.js.map
--- TypeScript/Understanding_TypeScript/Local_Testing_Environment/classes.ts
+++ TypeScript/Understanding_TypeScript/Local_Testing_Environment/classes.ts
@@ -1,12 +1,26 @@
 class Person {
     public name: string;
+    public username: string;
     private type: string;
-    protected age: number;
+    protected age: number = 21;
 
-    constructor(name: string, public username: string) {
+    constructor(name: string, username: string) {
         this.name = name;
+        this.username = username;
     }
+
+    printAge() {
+        console.log(this.age);
+    }
+
+    setType(type: string) {
+        this.type = type;
+        console.log(this.type);
+    }
+
 }
 
-const person = new Person("Armando", "SekethThoth");
+const person = new Person("Armando", "SekethThoth369");
 console.log(person);
+person.printAge();
+person.setType("Cool guy");
