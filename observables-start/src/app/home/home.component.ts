503b77157ea5f63e234f41dee67baedba636707b
Armando Alexis Herra Cortez <armandoherra369@gmail.com>
2017-06-17 04:40:39 -0500

    Finished working on the files for the Observables section.

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {Observer} from 'rxjs/Observer';
+import 'rxjs/Rx';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  numbersObservableSubscription: Subscription;
+  customObservable: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    const myNumbers = Observable.interval(1000).map(
+      (data: number) => {
+        return data * 2;
+      }
+    );
+
+    this.numbersObservableSubscription = myNumbers.subscribe(
+      (number: number) => {
+        console.log(number);
+      }
+    );
+
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('First Package');
+      }, 2000);
+      setTimeout(() => {
+        observer.next('Second Package');
+      }, 4000);
+      setTimeout(() => {
+        // observer.error('This Does Not Work!');
+        observer.complete();
+      }, 5000);
+      setTimeout(() => {
+        // observer.error('This Does Not Work!');
+        observer.next('Third Package');
+      }, 6000);
+    });
+
+    this.customObservable = myObservable.subscribe(
+      (data: string) => { console.log(data); },
+      (error: string) => { console.log(error); },
+      () => { console.log('Completed'); }
+    );
   }
 
+  ngOnDestroy() {
+    this.numbersObservableSubscription.unsubscribe();
+    this.customObservable.unsubscribe();
+  }
 }
---
+++ observables-start/src/app/users.service.ts
@@ -0,0 +1,8 @@
+import {Subject} from 'rxjs/Subject';
+
+
+
+export class UsersService {
+  userActivated = new Subject();
+
+}
