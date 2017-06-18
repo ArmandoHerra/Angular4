e7461222623b31336242e1edc102ef4d8be7ed83
Armando Alexis Herra Cortez <armandoherra369@gmail.com>
2017-06-17 20:00:02 -0500

    Finished the Template Driven Forms Assignment.

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  selectedSubscription = 'Advanced';
+  @ViewChild('signupForm') sgnForm: NgForm;
+
+  onSubmit() {
+    console.log(this.sgnForm.value);
+  }
 
 }
