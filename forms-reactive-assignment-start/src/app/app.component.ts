706fca5b40645ee76cd98699d0d6e839d8908748
Armando Alexis Herra Cortez <armandoherra369@gmail.com>
2017-06-20 02:11:04 -0500

    Finished the Reactive Forms Assignment.

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {CustomValidators} from './custom-validators';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    projectForm: FormGroup;
+
+    ngOnInit() {
+        this.projectForm = new FormGroup({
+           'projectName': new FormControl(
+               null,
+               [Validators.required, CustomValidators.invalidProjectName],
+               CustomValidators.asyncInvalidProjectName
+           ),
+            'email': new FormControl(
+                null,
+                [Validators.required, Validators.email]
+            ),
+            'projectStatus': new FormControl('finished')
+        });
+    }
+
+    onSaveProject() {
+        console.log(this.projectForm.value);
+    }
 }
